8cb38c024d4bf76ae847d1c588bb4629da6b115a
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:22:03 -0500

    872

---
+++ src/872. Leaf-Similar Trees.ts
@@ -0,0 +1,65 @@
+import assert from 'assert';
+import _ from 'lodash';
+
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+const funcs = [
+    function leafSimilar(root1: TreeNode | null, root2: TreeNode | null): boolean {
+        function getLeafs(node: TreeNode | null, leafs: number[]): number[] {
+            if (node === null) return leafs;
+            if (node.left === null && node.right === null) leafs.push(node.val);
+            getLeafs(node.left, leafs);
+            getLeafs(node.right, leafs);
+            return leafs;
+        }
+        const leafs1 = getLeafs(root1, []);
+        const leafs2 = getLeafs(root2, []);
+        return _.isEqual(leafs1, leafs2);
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
+}
+
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
