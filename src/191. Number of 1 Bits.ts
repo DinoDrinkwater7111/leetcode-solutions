f18be864b3f0478b6336587605914d84f33f98dd
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:20:54 -0500

    191

---
+++ src/191. Number of 1 Bits.ts
@@ -0,0 +1,42 @@
+import assert from 'assert';
+
+function hammingWeight(n: number): number {
+    let count: number;
+    if (n > 2147483647) {
+        count = n & 1;
+        n = Math.floor(n / 2);
+    } else {
+        count = 0;
+    }
+    while (n > 0) {
+        count += n & 1;
+        n >>= 1;
+    }
+    return count;
+}
+
+function _hammingWeight(n: number): number {
+    let count = 0;
+    while (n > 0) {
+        if (n % 2 === 1) count++;
+        n = Math.floor(n / 2);
+    }
+    return count;
+}
+
+function test() {
+    let n: number = 0;
+    try {
+        for (n = 0; n <= 1024; n++) {
+            assert.deepStrictEqual(hammingWeight(n), _hammingWeight(n));
+        }
+    } catch (e) {
+        console.log(n);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+    }
+}
+test();
