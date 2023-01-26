6fdb2c3e3b965c852b7f1b97b3d7ab4cef7aa886
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:37 -0500

    98

--- src/98. Validate Binary Search Tree.ts
+++ src/98. Validate Binary Search Tree.ts
@@ -1,4 +1,4 @@
-import assert from "assert";
+import assert from 'assert';
 
 class TreeNode {
     val: number;
@@ -11,55 +11,90 @@ class TreeNode {
     }
 }
 
-function isValidBST_(root: TreeNode | null): boolean {
-    function isValidBSTEx(node: TreeNode | null, lowerBound: number, upperBound: number): boolean {
-        if (node === null) return true;
-        if (node.val <= lowerBound || node.val >= upperBound) return false;
-        return isValidBSTEx(node.left, lowerBound, node.val) && isValidBSTEx(node.right, node.val, upperBound);
-    }
-
-    return isValidBSTEx(root, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
-}
-
-function isValidBST(root: TreeNode | null): boolean {
-    let curr = root;
-    let max = Number.NEGATIVE_INFINITY;
-    while (curr !== null) {
-        if (curr.left === null) {
-            if (max >= curr.val) return false;
-            max = curr.val;
-            curr = curr.right;
-        } else {
-            let traveler = curr.left;
-            while (traveler.right !== null && traveler.right !== curr) {
-                traveler = traveler.right;
-            }
-            if (traveler.right === null) {
-                traveler.right = curr;
-                curr = curr.left;
-            } else {
+const funcs = [
+    function isValidBST(root: TreeNode | null): boolean {
+        let curr = root;
+        let max = Number.NEGATIVE_INFINITY;
+        while (curr !== null) {
+            if (curr.left === null) {
                 if (max >= curr.val) return false;
                 max = curr.val;
-                traveler.right = null;
                 curr = curr.right;
+            } else {
+                let traveler = curr.left;
+                while (traveler.right !== null && traveler.right !== curr) {
+                    traveler = traveler.right;
+                }
+                if (traveler.right === null) {
+                    traveler.right = curr;
+                    curr = curr.left;
+                } else {
+                    if (max >= curr.val) return false;
+                    max = curr.val;
+                    traveler.right = null;
+                    curr = curr.right;
+                }
             }
         }
-    }
-    return true;
+        return true;
+    },
+    function isValidBST(root: TreeNode | null): boolean {
+        function isValidBSTEx(node: TreeNode | null, lowerBound: number, upperBound: number): boolean {
+            if (node === null) return true;
+            if (node.val <= lowerBound || node.val >= upperBound) return false;
+            return isValidBSTEx(node.left, lowerBound, node.val) && isValidBSTEx(node.right, node.val, upperBound);
+        }
+
+        return isValidBSTEx(root, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+    },
+    function isValidBST(root: TreeNode | null): boolean {
+        let max = Number.NEGATIVE_INFINITY;
+        function inOrderTravel(node: TreeNode | null): boolean {
+            if (node === null) return true;
+            const a = inOrderTravel(node.left);
+            if (max >= node.val) return false;
+            max = node.val;
+            const b = inOrderTravel(node.right);
+            return a && b;
+        }
+
+        return inOrderTravel(root);
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
 }
 
-function _isValidBST(root: TreeNode | null): boolean {
-    let max = Number.NEGATIVE_INFINITY;
-    function inOrderTravel(node: TreeNode | null): boolean {
-        if (node === null) return true;
-        const a = inOrderTravel(node.left);
-        if (max >= node.val) return false;
-        max = node.val;
-        const b = inOrderTravel(node.right);
-        return a && b;
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
     }
-
-    return inOrderTravel(root);
 }
 
-//TODO
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
