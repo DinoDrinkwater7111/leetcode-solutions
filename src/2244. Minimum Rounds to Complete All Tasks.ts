01995887000e2fc0df3d9545955079311123da83
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:22:05 -0500

    1833,2244

---
+++ src/2244. Minimum Rounds to Complete All Tasks.ts
@@ -0,0 +1,55 @@
+import assert from 'assert';
+
+const funcs = [
+    function minimumRounds(tasks: number[]): number {
+        const difficulty2count = new Map<number, number>();
+        for (let i = 0; i < tasks.length; i++) {
+            const difficulty = tasks[i];
+            const count = (difficulty2count.get(difficulty) ?? 0) + 1;
+            difficulty2count.set(difficulty, count);
+        }
+        let result = 0;
+        for (const count of difficulty2count.values()) {
+            if (count === 1) return -1;
+            result += Math.ceil(count / 3);
+        }
+        return result;
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
+}
+
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
