2c966b1984e8504b99fdb8e04c307d5828643e79
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:36 -0500

    108

--- src/108. Convert Sorted Array to Binary Search Tree.ts
+++ src/108. Convert Sorted Array to Binary Search Tree.ts
@@ -11,28 +11,64 @@ class TreeNode {
     }
 }
 
-function sortedArrayToBST(nums: number[]): TreeNode | null {
+const funcs = [
+    function sortedArrayToBST(nums: number[]): TreeNode | null {
+        const result = new TreeNode();
+        function sortedArrayToBSTEx(node: TreeNode, start: number, end: number, isLeft: boolean): void {
+            if (start > end) return;
+            if (isLeft) {
+                const mid = (start + end) >> 1;
+                node.left = new TreeNode(nums[mid]);
+                sortedArrayToBSTEx(node.left, start, mid - 1, true);
+                sortedArrayToBSTEx(node.left, mid + 1, end, false);
+            } else {
+                const mid = Math.ceil((start + end) / 2);
+                node.right = new TreeNode(nums[mid]);
+                sortedArrayToBSTEx(node.right, start, mid - 1, true);
+                sortedArrayToBSTEx(node.right, mid + 1, end, false);
+            }
+        }
+        const mid = (nums.length - 1) >> 1;
+        result.val = nums[mid];
+        sortedArrayToBSTEx(result, 0, mid - 1, true);
+        sortedArrayToBSTEx(result, mid + 1, nums.length - 1, false);
+        return result;
+    },
+];
 
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
 }
 
-function _sortedArrayToBST(nums: number[]): TreeNode | null {
-    const result = new TreeNode();
-    function sortedArrayToBSTEx(node: TreeNode, start: number, end: number, isLeft: boolean): void {
-        if (start > end) return;
-        if (isLeft) {
-            const mid = (start + end) >> 1;
-            node.left = new TreeNode(nums[mid]);
-            sortedArrayToBSTEx(node.left, start, mid - 1, true);
-            sortedArrayToBSTEx(node.left, mid + 1, end, false);
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
         } else {
-            const mid = Math.ceil((start + end) / 2);
-            node.right = new TreeNode(nums[mid]);
-            sortedArrayToBSTEx(node.right, start, mid - 1, true);
-            sortedArrayToBSTEx(node.right, mid + 1, end, false);
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
         }
     }
-    const mid = (nums.length - 1) >> 1;
-    result.val = nums[mid];
-    sortedArrayToBSTEx(result, 0, mid - 1, true);
-    sortedArrayToBSTEx(result, mid + 1, nums.length - 1, false);
 }
