5676fcbddbf518b23162856da78a56f216b84975
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:42 -0500

    342

---
+++ src/342. Power of Four.ts
@@ -0,0 +1,51 @@
+import assert from 'assert';
+
+const funcs = [
+    function isPowerOfFour(n: number): boolean {
+        return [
+            1, 4, 16, 64, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864, 268435456,
+            1073741824,
+        ].includes(n);
+    },
+    function isPowerOfFour(n: number): boolean {
+        if (n < 1) return false;
+        return 1 << ((Math.round(Math.log2(n)) >> 1) << 1) === n;
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
+}
+
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
