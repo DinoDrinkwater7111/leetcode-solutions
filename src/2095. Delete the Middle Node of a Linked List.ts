9fbec82e4000dc059eb5149bfd7387eade5ae40b
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:54 -0500

    2095

---
+++ src/2095. Delete the Middle Node of a Linked List.ts
@@ -0,0 +1,61 @@
+import assert from 'assert';
+
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+const funcs = [
+    function deleteMiddle(head: ListNode | null): ListNode | null {
+        if (head === null || head.next === null) return null;
+        let slow = head;
+        let fast = head.next;
+        while (fast.next !== null && fast.next.next !== null) {
+            slow = slow.next!;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next!.next;
+        return head;
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
+}
+
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
