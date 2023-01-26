aea3313b6a224835d1e6b8de713803197e56d6a5
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:36 -0500

    429

---
+++ src/429. N-ary Tree Level Order Traversal.ts
@@ -0,0 +1,60 @@
+import assert from 'assert';
+
+class Node {
+    val: number;
+    children: Node[];
+    constructor(val?: number) {
+        this.val = val === undefined ? 0 : val;
+        this.children = [];
+    }
+}
+
+const funcs = [
+    function levelOrder(root: Node | null): number[][] {
+        if (root === null) return [];
+        const result: number[][] = [];
+        let levelNodes = [root];
+        while (levelNodes.length > 0) {
+            result.push(levelNodes.map((node) => node.val));
+            levelNodes = levelNodes.map((node) => node.children).flat();
+        }
+        return result;
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
+}
+
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
