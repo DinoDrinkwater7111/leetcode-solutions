2d912efdd4f9fbdfe1e02eb08b6514d25034d88b
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:51 -0500

    557

--- src/557. Reverse Words in a String III.ts
+++ src/557. Reverse Words in a String III.ts
@@ -1,61 +1,98 @@
 import assert from 'assert';
 
-function reverseWords(s: string): string {
-    function reverse(arr: unknown[], start: number, end: number) {
-        if (start >= end) return;
-        for (let i = start; i <= Math.floor((start + end) / 2); i++) {
-            const temp = arr[i];
-            arr[i] = arr[end - i + start];
-            arr[end - i + start] = temp;
+const funcs = [
+    function reverseWords(s: string): string {
+        const resultArr: string[] = [];
+        let reverseCharArr: string[] = [];
+        for (let i = s.length - 1; i >= 0; i--) {
+            if (s[i] === ' ') {
+                resultArr.push(reverseCharArr.join(''), ' ');
+                reverseCharArr = [];
+            } else {
+                reverseCharArr.push(s[i]);
+            }
         }
-    }
-
-    let charArr: string[] = Array.from(s);
-    let currentWordInd = 0;
-    while (currentWordInd < charArr.length) {
-        while (charArr[currentWordInd] === ' ') {
-            currentWordInd++;
+        if (reverseCharArr.length > 0) resultArr.push(reverseCharArr.join(''));
+        return resultArr.reverse().join('');
+    },
+    function reverseWords(s: string): string {
+        function reverse(arr: unknown[], start: number, end: number) {
+            if (start >= end) return;
+            for (let i = start; i <= Math.floor((start + end) / 2); i++) {
+                const temp = arr[i];
+                arr[i] = arr[end - i + start];
+                arr[end - i + start] = temp;
+            }
         }
-        let i = currentWordInd + 1;
-        while (i < charArr.length && charArr[i] !== ' ') {
-            i++;
+
+        let charArr: string[] = Array.from(s);
+        let currentWordInd = 0;
+        while (currentWordInd < charArr.length) {
+            while (charArr[currentWordInd] === ' ') {
+                currentWordInd++;
+            }
+            let i = currentWordInd + 1;
+            while (i < charArr.length && charArr[i] !== ' ') {
+                i++;
+            }
+            reverse(charArr, currentWordInd, i - 1);
+            currentWordInd = i + 1;
         }
-        reverse(charArr, currentWordInd, i - 1);
-        currentWordInd = i + 1;
-    }
 
-    return charArr.join('');
-}
+        return charArr.join('');
+    },
+    function reverseWords(s: string): string {
+        return s
+            .split(' ')
+            .map((w) => Array.from(w).reverse().join(''))
+            .join(' ');
+    },
+];
 
-function _reverseWords(s: string): string {
-    return s
-        .split(' ')
-        .map((w) => Array.from(w).reverse().join(''))
-        .join(' ');
-}
-
-function test() {
-    let s: string = '';
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
     const pool = Array(10)
         .fill(undefined)
         .map((v, i) => i.toString());
     pool.push(' ');
-    try {
-        for (let i = 2; i < 1000; i++) {
-            s = Array(i)
+    for (let n = 0; n < 1000; n++) {
+        yield [
+            Array(n)
                 .fill(undefined)
                 .map(() => pool[Math.floor(Math.random() * pool.length)])
-                .join('');
-            assert.deepStrictEqual(reverseWords(s), _reverseWords(s));
-        }
+                .join(''),
+        ];
+    }
+}
+
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
     } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
         if (e instanceof assert.AssertionError) {
-            console.log(s);
             console.log(e.message);
         } else {
-            console.log(s);
             console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
         }
     }
 }
-test();
