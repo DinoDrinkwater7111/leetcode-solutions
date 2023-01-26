341ab4354a04a7f741c76baf11fe3db380b64f37
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:46 -0500

    322, 637

--- src/322. Coin Change.ts
+++ src/322. Coin Change.ts
@@ -1,73 +1,85 @@
 import assert from 'assert';
 
-function coinChange(coins: number[], amount: number): number {
-    coins = coins.filter((c) => c <= amount);
-    let dp: number[] = [0];
-    let count = 0;
-    const calculated: null[] = [];
-    while (dp.length > 0) {
-        const dp_: number[] = [];
-        for (const amt of dp) {
-            if (amt === amount) return count;
-            for (const coin of coins) {
-                const amt_ = amt + coin;
-                if (calculated[amt_] === undefined && amt_ <= amount) {
-                    dp_.push(amt_);
-                    calculated[amt_] = null;
+const funcs = [
+    function coinChange(coins: number[], amount: number): number {
+        coins = coins.filter((c) => c <= amount);
+        let dp: number[] = [0];
+        let count = 0;
+        const calculated = new Set<number>();
+        while (dp.length > 0) {
+            const dp_: number[] = [];
+            for (const amt of dp) {
+                if (amt === amount) return count;
+                for (const coin of coins) {
+                    const amt_ = amt + coin;
+                    if (!calculated.has(amt_) && amt_ <= amount) {
+                        dp_.push(amt_);
+                        calculated.add(amt_);
+                    }
                 }
             }
+            dp = dp_;
+            count++;
         }
-        dp = dp_;
-        count++;
-    }
-    return -1;
-}
-
-function _coinChange(coins: number[], amount: number): number {
-    coins = coins.filter((c) => c <= amount);
-    const dp: number[] = [];
-    function coinChangeEx(amt: number): number {
-        if (amt === 0) return 0;
-        if (amt < 0) return Number.POSITIVE_INFINITY;
-        if (dp[amt] === undefined) {
-            let min = Number.POSITIVE_INFINITY;
-            for (let c of coins) {
-                min = Math.min(min, coinChangeEx(amt - c));
-            }
-            dp[amt] = min + 1;
-        }
-        return dp[amt];
-    }
-    const result = coinChangeEx(amount);
-    if (Number.isFinite(result)) {
-        return result;
-    } else {
         return -1;
-    }
+    },
+    function coinChange(coins: number[], amount: number): number {
+        coins = coins.filter((c) => c <= amount);
+        const dp: number[] = [];
+        function coinChangeEx(amt: number): number {
+            if (amt === 0) return 0;
+            if (amt < 0) return Number.POSITIVE_INFINITY;
+            if (dp[amt] === undefined) {
+                let min = Number.POSITIVE_INFINITY;
+                for (let c of coins) {
+                    min = Math.min(min, coinChangeEx(amt - c));
+                }
+                dp[amt] = min + 1;
+            }
+            return dp[amt];
+        }
+        const result = coinChangeEx(amount);
+        if (Number.isFinite(result)) {
+            return result;
+        } else {
+            return -1;
+        }
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
 }
 
-function test() {
-    let coins: number[] = [];
-    let amount: number = 0;
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
     try {
-        for (let i = 2; i < 1000; i++) {
-            coins = Array(12)
-                .fill(undefined)
-                .map(() => Math.floor(Math.random() * 10000) + 1);
-            amount = Math.random() * 10 ** 4;
-            coins = [3, 7, 405, 436];
-            amount = 8839;
-            assert.deepStrictEqual(coinChange(coins, amount), _coinChange(coins, amount));
-            break;
-        }
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
     } catch (e) {
-        console.log(coins);
-        console.log(amount);
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
         if (e instanceof assert.AssertionError) {
             console.log(e.message);
         } else {
             console.error(e);
         }
+        return false;
     }
 }
-test();
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
---
+++ src/637. Average of Levels in Binary Tree.ts
@@ -0,0 +1,68 @@
+import assert from 'assert';
+
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+const funcs = [
+    function averageOfLevels(root: TreeNode | null): number[] {
+        const result: number[] = [];
+        let level = [root!];
+        while (level.length > 0) {
+            let sum = 0;
+            const level_: TreeNode[] = [];
+            for (const node of level) {
+                sum += node.val;
+                if (node.left !== null) level_.push(node.left);
+                if (node.right !== null) level_.push(node.right);
+            }
+            result.push(sum / level.length);
+            level = level_;
+        }
+        return result;
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
+}
+
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
