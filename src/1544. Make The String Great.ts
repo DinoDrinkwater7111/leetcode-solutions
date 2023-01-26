9fc8b5f04187cddd0236ebfd7e43dcb32390a173
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:58 -0500

    1544

---
+++ src/1544. Make The String Great.ts
@@ -0,0 +1,52 @@
+import assert from 'assert';
+
+const funcs = [
+    function makeGood(s: string): string {
+        const stack: string[] = [''];
+        for (let i = 0; i < s.length; i++) {
+            if (stack[stack.length - 1] !== s[i] && stack[stack.length - 1].toUpperCase() === s[i].toUpperCase()) {
+                stack.pop();
+            } else {
+                stack.push(s[i]);
+            }
+        }
+        return stack.join('');
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
+}
+
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
