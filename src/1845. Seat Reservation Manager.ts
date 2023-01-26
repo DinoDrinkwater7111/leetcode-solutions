799f8b29b2112edd037c8f56a811c6cdd66d7ce6
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:41 -0500

    1845

--- src/1845. Seat Reservation Manager.ts
+++ src/1845. Seat Reservation Manager.ts
@@ -1,6 +1,34 @@
 import assert from 'assert';
 
 class SeatManager {
+    private readonly availableSeats: number[] = [];
+
+    constructor(n: number) {
+        for (let i = n; i >= 1; i--) this.availableSeats.push(i);
+    }
+
+    private findInfInd(target: number): number {
+        let start = 0;
+        let end = this.availableSeats.length;
+        while (start < end) {
+            const mid = (start + end) >> 1;
+            if (target >= this.availableSeats[mid]) end = mid;
+            else start = mid + 1;
+        }
+        return start;
+    }
+
+    reserve(): number {
+        return this.availableSeats.pop()!;
+    }
+
+    unreserve(seatNumber: number): void {
+        const infInd = this.findInfInd(seatNumber);
+        this.availableSeats.splice(infInd, 0, seatNumber);
+    }
+}
+
+class _SeatManager {
     private readonly seats: number[] = [];
 
     constructor(n: number) {
