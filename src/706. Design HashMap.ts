ab9e55cc6a978b82ecc10733a34400b2a45ccf1c
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:30 -0500

    706

--- src/706. Design HashMap.ts
+++ src/706. Design HashMap.ts
@@ -1,33 +1,44 @@
 import assert from 'assert';
 
 class MyHashMap {
-    private readonly arr: [number, number][] = [];
+    private readonly keyArr: number[] = [];
+    private readonly valArr: number[] = [];
 
     constructor() {}
 
-    put(key: number, value: number): void {
-        let i = 0;
-        while (i < this.arr.length && this.arr[i][0] !== key) {
-            i++;
+    private findKeySupInd(target: number): number {
+        let start = 0;
+        let end = this.keyArr.length;
+        while (start < end) {
+            const mid = (start + end) >> 1;
+            if (target <= this.keyArr[mid]) end = mid;
+            else start = mid + 1;
         }
-        this.arr[i] = [key, value];
+        return start;
+    }
+
+    put(key: number, value: number): void {
+        const ind = this.findKeySupInd(key);
+        if (this.keyArr[ind] === key) {
+            this.valArr[ind] = value;
+        } else {
+            this.keyArr.splice(ind, 0, key);
+            this.valArr.splice(ind, 0, value);
+        }
     }
 
     remove(key: number): void {
-        for (let i = 0; i < this.arr.length; i++) {
-            if (this.arr[i][0] === key) {
-                [this.arr[i], this.arr[this.arr.length - 1]] = [this.arr[this.arr.length - 1], this.arr[i]];
-                this.arr.pop();
-                break;
-            }
+        const ind = this.findKeySupInd(key);
+        if (this.keyArr[ind] === key) {
+            this.keyArr.splice(ind, 1);
+            this.valArr.splice(ind, 1);
         }
     }
 
     get(key: number): number {
-        let i = 0;
-        while (i < this.arr.length && this.arr[i][0] !== key) {
-            i++;
-        }
-        return this.arr[i]?.[1] ?? -1;
+        const ind = this.findKeySupInd(key);
+        return this.keyArr[ind] === key ? this.valArr[ind] : -1;
     }
 }
+
+//TODO
