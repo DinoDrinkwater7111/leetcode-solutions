8dfc2e1e296432b3d016c7c843c7a996905bb9cb
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:22:04 -0500

    70

--- src/70. Climbing Stairs.ts
+++ src/70. Climbing Stairs.ts
@@ -1,47 +1,77 @@
 import assert from 'assert';
 
-function climbStairs(n: number): number {
-    const factorialCache = [1];
-    function factorial(n: number): number {
-        if (factorialCache[n] !== undefined) {
-            return factorialCache[n];
-        } else {
-            const result = n * factorial(n - 1);
-            factorialCache.push();
-            return result;
+const funcs = [
+    function climbStairs(n: number): number {
+        let _dp = 1;
+        let dp = 1;
+        for (let i = 2; i <= n; i++) {
+            const temp = dp;
+            dp += _dp;
+            _dp = temp;
         }
-    }
+        return dp;
+    },
+    function climbStairs(n: number): number {
+        const factorialCache = [1];
+        function factorial(n: number): number {
+            if (factorialCache[n] !== undefined) {
+                return factorialCache[n];
+            } else {
+                const result = n * factorial(n - 1);
+                factorialCache.push();
+                return result;
+            }
+        }
 
-    let sum = 0;
-    for (let k = 0; k <= n / 2; k++) {
-        sum += factorial(n - k) / (factorial(k) * factorial(n - 2 * k));
-    }
+        let sum = 0;
+        for (let k = 0; k <= n / 2; k++) {
+            sum += factorial(n - k) / (factorial(k) * factorial(n - 2 * k));
+        }
 
-    return sum;
+        return sum;
+    },
+    function climbStairs(n: number): number {
+        const cache = [1, 1];
+        for (let k = 2; k <= n; k++) {
+            cache[k] = cache[k - 1] + cache[k - 2];
+        }
+        return cache[n];
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
 }
 
-function _climbStairs(n: number): number {
-    const cache = [1, 1];
-    for (let k = 2; k <= n; k++) {
-        cache[k] = cache[k - 1] + cache[k - 2];
-    }
-    return cache[n];
-}
-
-function test() {
-    let nums: number = 0;
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
     try {
-        for (let i = 1; i <= 37; i++) {
-            assert.strictEqual(climbStairs(i),_climbStairs(i))
-        }
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
     } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
         if (e instanceof assert.AssertionError) {
-            console.log(nums);
             console.log(e.message);
         } else {
-            console.log(nums);
             console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
         }
     }
 }
-test();
