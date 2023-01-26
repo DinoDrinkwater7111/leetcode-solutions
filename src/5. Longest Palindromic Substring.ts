9371ac65d855ec67752eae908603db870c3dad90
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:00 -0500

    5

--- src/5. Longest Palindromic Substring.ts
+++ src/5. Longest Palindromic Substring.ts
@@ -1,8 +1,36 @@
 import assert from 'assert';
 
-export {};
-
 function longestPalindrome(s: string): string {
+    const cache: boolean[] = [true];
+    let result: string = s[0];
+    function checkLengthAndReplace(i: number, j: number): void {
+        if (result.length < j - i + 1) result = s.substring(i, j + 1);
+    }
+    for (let i = s.length - 2; i >= 0; i--) {
+        cache.push(true); //add length to cache only
+        for (let j = s.length - 1; j > i; j--) {
+            const diff = s.length - 1 - j;
+            if (j == i + 1) {
+                if (s[i] === s[j]) {
+                    checkLengthAndReplace(i, j);
+                    cache[cache.length - 1 - diff] = true;
+                } else {
+                    cache[cache.length - 1 - diff] = false;
+                }
+            } else {
+                if (s[i] === s[j] && cache[cache.length - 1 - diff - 2]) {
+                    cache[cache.length - 1 - diff] = true;
+                    checkLengthAndReplace(i, j);
+                } else {
+                    cache[cache.length - 1 - diff] = false;
+                }
+            }
+        }
+    }
+    return result;
+}
+
+function _longestPalindrome(s: string): string {
     const charArr = Array.from(s);
     let candidateStart = 0;
     let candidateEnd = 0;
@@ -31,7 +59,7 @@ function longestPalindrome(s: string): string {
     return charArr.slice(candidateStart, candidateEnd + 1).join('');
 }
 
-function _longestPalindrome(s: string): string {
+function __longestPalindrome(s: string): string {
     const charArr = Array.from(s);
     let candidate: string = charArr[0];
 
@@ -70,10 +98,11 @@ function test() {
                 .map(() => charPool[Math.floor(Math.random() * charPool.length)])
                 .join('');
             assert.equal(longestPalindrome(s), _longestPalindrome(s));
+            assert.equal(_longestPalindrome(s), __longestPalindrome(s));
         }
     } catch (e) {
+        console.log(s);
         if (e instanceof assert.AssertionError) {
-            console.log(s);
             console.log(e.message);
         } else {
             console.error(e);
