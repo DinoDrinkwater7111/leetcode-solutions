d52740730142cba21bbfd664d5874b3090d184e6
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:40 -0500

    61

---
+++ src/61. Rotate List.ts
@@ -0,0 +1,65 @@
+import assert from 'assert';
+
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+const funcs = [
+    function rotateRight(head: ListNode | null, k: number): ListNode | null {
+        if (head === null) return null;
+        let count = 1;
+        let curr = head;
+        while (curr.next !== null) {
+            count++;
+            curr = curr.next;
+        }
+        curr.next = head;
+        curr = head;
+        for (let i = count - (k % count) - 1; i > 0; i--) {
+            curr = curr.next!;
+        }
+        const result = curr.next;
+        curr.next = null;
+        return result;
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {}
+
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
