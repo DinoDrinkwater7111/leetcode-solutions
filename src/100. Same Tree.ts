fccb8461f6db653fbc80b7a8fb48dc8d5bc84ec5
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:47 -0500

    100, 101

---
+++ src/100. Same Tree.ts
@@ -0,0 +1,59 @@
+import assert from 'assert';
+
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+const funcs = [
+    function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+        if (p === null && q === null) return true;
+        if (p !== null && q !== null){
+            return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
+}
+
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
--- src/101. Symmetric Tree.ts
+++ src/101. Symmetric Tree.ts
@@ -1,4 +1,4 @@
-import assert from "assert";
+import assert from 'assert';
 
 class TreeNode {
     val: number;
@@ -11,57 +11,92 @@ class TreeNode {
     }
 }
 
-function isSymmetric(root: TreeNode | null): boolean {
-    let _node = root!.left;
-    let node_ = root!.right;
-    while (_node !== null && node_ !== null) {
-        if (_node.val !== node_.val) return false;
-        if (_node.left === null && node_.right === null) {
-            _node = _node.right;
-            node_ = node_.left;
-        } else if (_node.left !== null && node_.right !== null) {
-            let _traveler = _node.left;
-            let traveler_ = node_.right;
-            while (_traveler.right !== null && _traveler.right !== _node) {
-                if (!(traveler_.left !== null && traveler_.left !== node_)) return false;
-                _traveler = _traveler.right;
-                traveler_ = traveler_.left;
-            }
-            if (_traveler.right === null && traveler_.left === null) {
-                _traveler.right = _node;
-                _node = _node.left;
-                traveler_.left = node_;
-                node_ = node_.right;
-            } else if (_traveler.right === _node && traveler_.left === node_) {
-                _traveler.right = null;
+const funcs = [
+    function isSymmetric(root: TreeNode | null): boolean {
+        let _node = root!.left;
+        let node_ = root!.right;
+        while (_node !== null && node_ !== null) {
+            if (_node.val !== node_.val) return false;
+            if (_node.left === null && node_.right === null) {
                 _node = _node.right;
-                traveler_.left = null;
                 node_ = node_.left;
+            } else if (_node.left !== null && node_.right !== null) {
+                let _traveler = _node.left;
+                let traveler_ = node_.right;
+                while (_traveler.right !== null && _traveler.right !== _node) {
+                    if (!(traveler_.left !== null && traveler_.left !== node_)) return false;
+                    _traveler = _traveler.right;
+                    traveler_ = traveler_.left;
+                }
+                if (_traveler.right === null && traveler_.left === null) {
+                    _traveler.right = _node;
+                    _node = _node.left;
+                    traveler_.left = node_;
+                    node_ = node_.right;
+                } else if (_traveler.right === _node && traveler_.left === node_) {
+                    _traveler.right = null;
+                    _node = _node.right;
+                    traveler_.left = null;
+                    node_ = node_.left;
+                } else {
+                    return false;
+                }
             } else {
                 return false;
             }
-        } else {
+        }
+
+        return _node === node_;
+    },
+    function isSymmetric(root: TreeNode | null): boolean {
+        function isSymmetricEx(_node: TreeNode | null, node_: TreeNode | null): boolean {
+            if (_node === null && node_ === null) return true;
+            if (_node !== null && node_ !== null)
+                return (
+                    _node.val === node_.val &&
+                    isSymmetricEx(_node.left, node_.right) &&
+                    isSymmetricEx(_node.right, node_.left)
+                );
             return false;
         }
-    }
 
-    return _node === node_;
+        return isSymmetricEx(root!.left, root!.right);
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
 }
 
-function _isSymmetric(root: TreeNode | null): boolean {
-    function isSymmetricEx(_node: TreeNode | null, node_: TreeNode | null): boolean {
-        if (_node === null && node_ === null) {
-            return true;
-        } else if (_node !== null && node_ !== null) {
-            if (_node.val !== node_.val) return false;
-            return isSymmetricEx(_node.left, node_.right) && isSymmetricEx(_node.right, node_.left);
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
         } else {
-            return false;
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
         }
     }
-
-    return isSymmetricEx(root!.left, root!.right);
 }
-
-
-//TODO
