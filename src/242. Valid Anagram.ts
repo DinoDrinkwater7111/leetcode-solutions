1afbd3ee661c05c7228b8a7915b7bd136a4d0d93
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:09 -0500

    242

---
+++ src/242. Valid Anagram.ts
@@ -0,0 +1,47 @@
+import assert from 'assert';
+
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) return false;
+    const codePoint2Count: number[] = [];
+    for (let i = 97; i < 123; i++) codePoint2Count[i] = 0;
+    for (const char of t) {
+        const codePoint = char.codePointAt(0)!;
+        codePoint2Count[codePoint]++;
+    }
+    for (const char of s) {
+        const codePoint = char.codePointAt(0)!;
+        if (codePoint2Count[codePoint] === 0) return false;
+        else codePoint2Count[codePoint]--;
+    }
+
+    return true;
+}
+
+function _isAnagram(s: string, t: string): boolean {}
+
+//TODO
+function test() {
+    let nums: number[] = [];
+    let target: number = 0;
+    try {
+        for (let i = 2; i < 1000; i++) {
+            nums = Array(i)
+                .fill(undefined)
+                .map(() => Math.random() - 0.5);
+            const i1 = Math.floor(Math.random() * i);
+            let i2 = Math.floor(Math.random() * i);
+            while (i2 === i1) i2 = Math.floor(Math.random() * i);
+            target = nums[i1] + nums[i2];
+            assert.deepStrictEqual(twoSum(nums, target), _twoSum(nums, target));
+        }
+    } catch (e) {
+        console.log(nums);
+        console.log(target);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+    }
+}
+test();
