27b1173eb3ea91ee60bca4723fa541d660e23a05
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:50 -0500

    985

---
+++ src/985. Sum of Even Numbers After Queries.ts
@@ -0,0 +1,54 @@
+import assert from 'assert';
+
+const funcs = [
+    function sumEvenAfterQueries(nums: number[], queries: number[][]): number[] {
+        const result: number[] = [];
+        let sum = 0;
+        for (const num of nums) if ((num & 1) === 0) sum += num;
+        let sum = nums.reduce((p, c) => ((c & 1) === 0 ? p + c : p), 0);
+        for (const [val, i] of queries) {
+            if ((nums[i] & 1) === 0) sum += (val & 1) === 0 ? val : -nums[i];
+            else sum += (val & 1) === 1 ? nums[i] + val : 0;
+            nums[i] += val;
+            result.push(sum);
+        }
+        return result;
+    },
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
+}
+
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        assert.deepStrictEqual(funcs[actualFuncInd](...testCase), funcs[expectedFuncInd](...testCase));
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
+    }
+}
+
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
