727c471b60d01733f20fbb9b7a432fe414b200ea
GaryChanCCO <garychancco@gmail.com>
2023-01-26 03:21:54 -0500

    976

--- src/976. Largest Perimeter Triangle.ts
+++ src/976. Largest Perimeter Triangle.ts
@@ -1,19 +1,52 @@
-import assert from "assert";
+import assert from 'assert';
 
-function largestPerimeter(nums: number[]): number {
-    nums.sort((a, b) => b - a);
-    for (let i = 0; i < nums.length - 2; i++) {
-        if (nums[i] < nums[i + 1] + nums[i + 2]) return nums[i] + nums[i + 1] + nums[i + 2];
+const funcs = [
+    function largestPerimeter(nums: number[]): number {
+        nums.sort((a, b) => b - a);
+        for (let i = 0; i < nums.length - 2; i++) {
+            if (nums[i] < nums[i + 1] + nums[i + 2]) return nums[i] + nums[i + 1] + nums[i + 2];
+        }
+        return 0;
     }
-    return 0;
+];
+
+type TestCase = Parameters<typeof funcs[number]>;
+function* testCaseIterator(): Generator<TestCase> {
+    //TODO
 }
 
-function _largestPerimeter(nums: number[]): number {
-    nums.sort((a, b) => b - a);
-    for (let i = 0; i < nums.length - 2; i++) {
-        if (nums[i] < nums[i + 1] + nums[i + 2]) return nums[i] + nums[i + 1] + nums[i + 2];
+function test(testCase: TestCase, actualFuncInd: number, expectedFuncInd: number): boolean {
+    try {
+        const json = JSON.stringify(testCase);
+        assert.deepStrictEqual(
+            funcs[actualFuncInd](...(JSON.parse(json) as TestCase)),
+            funcs[expectedFuncInd](...(JSON.parse(json) as TestCase))
+        );
+        return true;
+    } catch (e) {
+        console.log('❌'.repeat(32));
+        console.log(`actualFuncInd: ${actualFuncInd}`);
+        console.log(`expectedFuncInd: ${expectedFuncInd}`);
+        console.log(`testCase: ${JSON.stringify(testCase)}`);
+        if (e instanceof assert.AssertionError) {
+            console.log(e.message);
+        } else {
+            console.error(e);
+        }
+        return false;
     }
-    return 0;
 }
 
-//TODO
+{
+    let count = 0;
+    outer: for (const testCase of testCaseIterator()) {
+        if (++count < 10) {
+            console.log('----------------------------------------------------');
+            console.log(`Testcase ${count}:`);
+            console.log(JSON.stringify(testCase, undefined, 2));
+        }
+        for (let i = 0; i < funcs.length - 1; i++) {
+            if (!test(testCase, i, i + 1)) break outer;
+        }
+    }
+}
